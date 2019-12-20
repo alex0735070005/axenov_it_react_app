9cc3ec2a3f46a94e6e3533808d57568a43541e6b
alex0735070005 <alex0735070005@gmail.com>
2019-12-20 02:27:34 +0200

    final-eslint

--- src/config/App.jsx
+++ src/config/App.jsx
@@ -6,17 +6,17 @@ import Footer from '../components/modules/Footer';
 import '../components/shared/styles.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <Header />
-        <Paper className="page">
-          <Router />
-        </Paper>
-        <Footer />
-      </div>
+	return (
+  <div className="App">
+    <div className="container">
+      <Header />
+      <Paper className="page">
+        <Router />
+      </Paper>
+      <Footer />
     </div>
-  );
+  </div>
+	);
 }
 
 export default App;
