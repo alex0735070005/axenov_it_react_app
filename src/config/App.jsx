c0cb1bfcc84ab9e03c1fe2e5d1a23258b4830eab
alex0735070005 <alex0735070005@gmail.com>
2019-12-20 03:21:42 +0200

    v3

--- src/config/App.jsx
+++ src/config/App.jsx
@@ -6,17 +6,19 @@ import Footer from '../components/modules/Footer';
 import '../components/shared/styles.scss';
 
 function App() {
-	return (
-  <div className="App">
-    <div className="container">
-      <Header />
-      <Paper className="page">
-        <Router />
-      </Paper>
-      <Footer />
+  return (
+    <div className="App">
+      {' '}
+      <div className="container">
+        {' '}
+        <Header />{' '}
+        <Paper className="page">
+          <Router />
+        </Paper>
+        <Footer />
+      </div>
     </div>
-  </div>
-	);
+  );
 }
 
 export default App;
